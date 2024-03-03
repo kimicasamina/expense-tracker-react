b086e821410b793c3e61b69b11a9f6a73e7d55a1
kimi.casamina <kimi.casamina@gmail.com>
2024-03-03 22:30:06 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="layout">
+        <h1 className='text-red-800'>HELLO WORLD</h1>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quos.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
